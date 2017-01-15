257bcd0ded810abd1414c23aa61dd77c8d81a9f8
Patrick Pacher <paz@ppacher.at>
2017-01-15 20:11:13 +0100

    Finished consul factory

--- providers/consul/consul.go
+++ providers/consul/consul.go
@@ -1,6 +1,9 @@
 package consul
 
 import (
+	"errors"
+	"fmt"
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/context"
@@ -28,6 +31,30 @@ func (consul *KV) Set(ctx context.Context, key string, value []byte) error {
 }
 
 func (consul *KV) Get(ctx context.Context, key string) (*kv.Node, error) {
+	pairs, _, err := consul.kv.List(key, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%q: %#v %#v \n", key, pairs, err)
+
+	if pairs == nil {
+		return nil, errors.New("no such key")
+	}
+
+	var node kv.Node
+
+	node.Key = key
+
+	for _, pair := range pairs {
+		var child kv.Node
+
+		child.Key = pair.Key
+		child.Value = pair.Value
+
+		node.Children = append(node.Children, child)
+	}
+
 	return nil, nil
 }
 
@@ -46,10 +73,33 @@ func sanatizeKey(key string) string {
 	return strings.Trim(key, "/ ")
 }
 
-func New(params map[string]string) (kv.KV, error) {
-	return &KV{}, nil
+func New(params map[string]string) (kv.Provider, error) {
+	config := api.DefaultConfig()
+
+	if v, ok := params["endpoint"]; ok && v != "" {
+		url, err := url.Parse(v)
+		if err != nil {
+			return nil, err
+		}
+		config.Address = url.Host
+		config.Scheme = url.Scheme
+	}
+
+	client, err := api.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+
+	kvapi := client.KV()
+
+	return &KV{
+		kv:  kvapi,
+		cli: client,
+	}, nil
 }
 
 func init() {
-	kv.Register("consul", New)
+	if err := kv.Register("consul", New, nil, []string{"endpoint"}); err != nil {
+		panic("failed to register consul KV driver")
+	}
 }
